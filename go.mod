2da37faa987d729e39e323586a9eb9271f835277
jake.0208(조재덕)/kakao <jake.0208@kakaocorp.com>
2022-05-17 17:24:11 +0900

    TEST: redis Hash Datatype test

--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module redis-test
 go 1.18
 
 require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/go-redis/redis/v8 v8.11.5
+)
+
+require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/go-redis/redis/v8 v8.11.5 // indirect
 )
--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/go-redis/redis/v8"
+	"log"
 	"os"
 )
 
@@ -19,17 +21,25 @@ func main() {
 	})
 
 	var ctx = context.Background()
+	_ = rdb.FlushDB(ctx).Err()
 
-	err := rdb.Set(ctx, "jake", "test", 0).Err()
+	//testStringType(rdb, ctx)
+
+	testHashType(rdb, ctx)
+
+}
+
+func testStringType(rdb *redis.Client, ctx context.Context) {
+	err := rdb.Set(ctx, "key1", "value1", 0).Err()
 	if err != nil {
 		panic(err)
 	}
 
-	val, err := rdb.Get(ctx, "jake").Result()
+	val, err := rdb.Get(ctx, "key1").Result()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("jake", val)
+	fmt.Println("key1:", val)
 
 	val2, err := rdb.Get(ctx, "key2").Result()
 	if err == redis.Nil {
@@ -37,8 +47,44 @@ func main() {
 	} else if err != nil {
 		panic(err)
 	} else {
-		fmt.Println("key2", val2)
+		fmt.Println("key2: ", val2)
 	}
-	// Output: key value
-	// key2 does not exist
+}
+
+func testHashType(rdb *redis.Client, ctx context.Context) {
+	// Set some fields.
+	if _, err := rdb.Pipelined(ctx, func(rdb redis.Pipeliner) error {
+		rdb.HSet(ctx, "key", "str1", "hello")
+		rdb.HSet(ctx, "key", "str2", "world")
+		rdb.HSet(ctx, "key", "int", 123)
+		rdb.HSet(ctx, "key", "bool", true)
+		return nil
+	}); err != nil {
+		panic(err)
+	}
+
+	var model1, model2 Model
+
+	// Scan all fields into the model.
+	if err := rdb.HGetAll(ctx, "key").Scan(&model1); err != nil {
+		panic(err)
+	}
+
+	// Or scan a subset of the fields.
+	if err := rdb.HMGet(ctx, "key", "str1", "int").Scan(&model2); err != nil {
+		panic(err)
+	}
+
+	log.Println(model1)
+	log.Println(model2)
+
+	spew.Dump(model1)
+	spew.Dump(model2)
+}
+
+type Model struct {
+	Str1 string `redis:"str1"`
+	Str2 string `redis:"str2"`
+	Int  int    `redis:"int"`
+	Bool bool   `redis:"bool"`
 }
